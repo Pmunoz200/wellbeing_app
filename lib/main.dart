5712015c00eb0b0b6077c50bcc66519eb8f49a09
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-06 00:45:17 +0200

    Changed app theme

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
       home: HomePage(),
