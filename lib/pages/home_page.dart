3482b3b041b91036d62120c569359136aa28d314
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-06 00:40:14 +0200

    Made the bottom navigate change the current page info

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    const Row(
+      children: [
+        Icon(Icons.fitness_center),
+        Text('Exercise'),
+      ],
+    ),
+    const Row(
+      children: [
+        Icon(Icons.fastfood),
+        Text('Food'),
+      ],
+    ),
+    const Row(
+      children: [
+        Icon(Icons.home),
+        Text('Home'),
+      ],
+    ),
+    const Row(
+      children: [
+        Icon(Icons.person),
+        Text('Profile'),
+      ],
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,26 +50,32 @@ class HomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Center(
-        child: Text(
-          'Home Page',
-          style: TextStyle(fontSize: 24),
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
         items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Excersice',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            label: 'Food',
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.person),
+            label: 'Profile',
           ),
         ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       )
     );
   }
