47f1cc83c2100241fbe557f6162357228ba1a9a3
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-16 15:27:04 +0200

    Adjusted widget to comply with new container definition

--- lib/pages/home_page/home_widget.dart
+++ lib/pages/home_page/home_widget.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:gemini_folder/pages/widgets/text_response_container.dart';
 
 const String tryText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap.";
+const List<String> textList = [tryText, "1" + tryText, "2" + tryText];
 class HomeWidgetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            TextResponseContainer(text: tryText),
-            TextResponseContainer(text: tryText),
+            TextResponseContainer(texts: textList, title: "Summary",),
+            TextResponseContainer(texts: textList, title: "Suggestions",),
           ],
       );
   }
