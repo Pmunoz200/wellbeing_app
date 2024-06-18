139dd8e7a3691a7c6d22dbda454764d886a07072
JuanJoseJ <s301352@polito.studenti.it>
2024-06-18 15:01:33 -0500

    Implement the base profile page and include it in the navigation system

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,12 +4,13 @@ import 'package:gemini_folder/pages/home_page/food_widget.dart';
 import 'package:gemini_folder/pages/home_page/home_widget.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _selectedIndex = 0;
 
   final List<Widget> _widgetOptions = <Widget>[
@@ -27,42 +28,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('WELLBEING APP'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              // Navigate to the notification page
-            },
-            color: Colors.lightBlueAccent,
-          ),],
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.lightBlueAccent,
-        unselectedItemColor: Colors.grey[400],
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Exersice',
-            backgroundColor: Theme.of(context).colorScheme.primary,
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            label: 'Food',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      )
-    );
+        appBar: AppBar(
+          title: const Text('WELLBEING APP'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                Navigator.pushNamed(context, "/profile");
+              },
+              color: Colors.lightBlueAccent,
+            ),
+          ],
+          backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.lightBlueAccent,
+          unselectedItemColor: Colors.grey[400],
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.fitness_center),
+              label: 'Exersice',
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood),
+              label: 'Food',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ));
   }
 }
