f8e2e28a491bda987b82ef8b7881a5d861e84c90
JuanJoseJ <s301352@polito.studenti.it>
2024-06-18 15:01:33 -0500

    Implement the base profile page and include it in the navigation system

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gemini_folder/firebase/firebase_options.dart';
 import 'package:gemini_folder/onboarding/onboarding.dart';
-import 'package:gemini_folder/token_page.dart';
+import 'package:gemini_folder/pages/profile_page/profile_widget.dart';
 import 'package:gemini_folder/user_authentication/login.dart';
 import 'pages/home_page.dart';
 
@@ -33,9 +33,10 @@ class MyApp extends StatelessWidget {
         "/authentication": (context) => LoginScreen(
               navigator: mainNavigatorKey,
             ),
-        '/token': (context) => TokenPage(navigator: mainNavigatorKey),
         '/onboarding': (context) =>
             OnboardingScreen(navigator: mainNavigatorKey),
+        '/home': (context) => const HomePage(),
+        '/profile': (context) => ProfileWidgetPage(navigator: mainNavigatorKey),
       },
     );
   }
