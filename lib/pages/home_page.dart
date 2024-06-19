7f756de53c69a2b1a54a0589d75e5874a72f5ab1
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-19 22:58:09 +0200

    Added first iteration of date on appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import the intl package
 import 'package:gemini_folder/pages/home_page/exercise_widget.dart';
 import 'package:gemini_folder/pages/home_page/food_widget.dart';
 import 'package:gemini_folder/pages/home_page/home_widget.dart';
@@ -11,6 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   int _selectedIndex = 0;
+  DateTime _selectedDate = DateTime.now();
 
   final List<Widget> _widgetOptions = <Widget>[
     HomeWidgetPage(),
@@ -24,11 +26,50 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _advanceOneDayOnDate () {
+    setState(() {
+      _selectedDate = _selectedDate.add(const Duration(days: 1));
+    });
+  }
+
+  void _goBackOneDayOnDate () {
+    setState(() {
+      _selectedDate = _selectedDate.subtract(const Duration(days: 1));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WELLBEING APP'),
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+          // Handle previous date logic
+              _goBackOneDayOnDate();
+              },
+              color: Colors.black,
+            ),
+            Text(
+              DateFormat('dd/MM/yyyy').format(_selectedDate).toString(), // Fix the DateTime.format method
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                // Handle next date logic
+                _selectedDate == DateTime.now() ? null : _advanceOneDayOnDate();
+              },
+              color: Colors.black,
+              )
+          ],
+        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.person),
