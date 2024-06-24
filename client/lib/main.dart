b43eb858abd4d1da74d3d6693137d8598cf16b74
JuanJoseJ <s301352@polito.studenti.it>
2024-06-24 07:13:01 -0500

    Feat: Add the display of the current values of the profile as a list

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gemini_folder/firebase/firebase_options.dart';
 import 'package:gemini_folder/onboarding/onboarding.dart';
+import 'package:gemini_folder/pages/profile_page/profile_widget.dart';
+import 'package:gemini_folder/providers/main_provider.dart';
 import 'package:gemini_folder/user_authentication/login.dart';
+import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
 void main() async {
@@ -10,7 +13,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => MainProvider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +26,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      // theme: AppThemeData.getThemeData(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
       initialRoute: '/authentication',
       navigatorKey: mainNavigatorKey,
       routes: {
@@ -32,7 +39,7 @@ class MyApp extends StatelessWidget {
         '/onboarding': (context) =>
             OnboardingScreen(navigator: mainNavigatorKey),
         '/home': (context) => const HomePage(),
-        // '/profile': (context) => ProfileWidgetPage(navigator: mainNavigatorKey),
+        '/profile': (context) => ProfileWidgetPage(navigator: mainNavigatorKey),
       },
     );
   }
---
+++ client/lib/pages/profile_page/profile_data_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:gemini_folder/onboarding/question_class.dart';
+import 'package:gemini_folder/user_authentication/profile_class.dart';
+
+class ProfileDataList extends StatelessWidget {
+  final List<OnboardingQuestion> profileQuestions;
+  final Profile userProfile;
+
+  const ProfileDataList(
+      {super.key, required this.profileQuestions, required this.userProfile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: profileQuestions.length,
+        itemBuilder: (context, index) {
+          String? value;
+          List<String>? listValues;
+          var parameterValue =
+              userProfile[profileQuestions[index].parameterName];
+
+          if (parameterValue is String) {
+            value = parameterValue;
+          } else if (parameterValue is int || parameterValue is double) {
+            value = parameterValue.toString();
+          } else if (parameterValue is List<String>) {
+            listValues = parameterValue;
+          }
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed("/onboarding", arguments: index);
+            },
+            child: listValues == null
+                ? ListTile(
+                    title: Text(profileQuestions[index].question),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Text(
+                        value ?? "",
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  )
+                : ExpansionTile(
+                    title: Text(profileQuestions[index].question),
+                    children: listValues
+                        .map((item) => ListTile(
+                            title: Text(item, overflow: TextOverflow.ellipsis)))
+                        .toList(),
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
