c26d024ce1ec7f2d149a8b5652dfad971d436f67
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-24 15:45:39 +0200

    Added home page styling and layout

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import the intl package
 import 'package:gemini_folder/pages/home_page/exercise_widget.dart';
 import 'package:gemini_folder/pages/home_page/food_widget.dart';
 import 'package:gemini_folder/pages/home_page/home_widget.dart';
@@ -11,10 +12,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   int _selectedIndex = 0;
+  DateTime _selectedDate = DateTime.now();
 
   final List<Widget> _widgetOptions = <Widget>[
-    ExerciseWidgetPage(),
     HomeWidgetPage(),
+    ExerciseWidgetPage(),
     FoodWidgetPage(),
   ];
 
@@ -24,45 +26,110 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _advanceOneDayOnDate () {
+    setState(() {
+      _selectedDate = _selectedDate.add(const Duration(days: 1));
+    });
+  }
+
+  void _goBackOneDayOnDate () {
+    setState(() {
+      _selectedDate = _selectedDate.subtract(const Duration(days: 1));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WELLBEING APP'),
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+          // Handle previous date logic
+              _goBackOneDayOnDate();
+              },
+              color: Colors.black,
+            ),
+            Text(
+              DateFormat('dd/MM/yyyy').format(_selectedDate).toString(), // Fix the DateTime.format method
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                // Handle next date logic
+                _selectedDate == DateTime.now() ? null : _advanceOneDayOnDate();
+              },
+              color: Colors.black,
+              )
+          ],
+        ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.person),
-            onPressed: () {
-              // Navigate to the notification page
-            },
-            color: Colors.lightBlueAccent,
-          ),],
+           MaterialButton(
+          onPressed: () {},
+          color: Colors.black,
+          textColor: Colors.white,
+          shape: CircleBorder(),
+          child: const Icon(
+            Icons.person,
+            size: 30,
+          ),
+        )],
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Colors.white,
+              Colors.white,
+            ],
+          ),
+        ),
+        child: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.lightBlueAccent,
-        unselectedItemColor: Colors.grey[400],
-        items: <BottomNavigationBarItem>[
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+        top: BorderSide(
+          color: Colors.black,
+          width: 0.5,
+        ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey[400],
+          items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Exersice',
-            backgroundColor: Theme.of(context).colorScheme.primary,
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            label: 'Food',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fitness_center),
+          label: 'Exercise',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fastfood),
+          label: 'Food',
+        ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       )
     );
   }
-}
+  }
