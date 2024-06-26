c3baaf8be31ff22499383738fcbe3978d17fda71
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-26 15:27:32 +0200

    Fixed minor problem with router path

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
         '/token': (context) => HomePage(),
         '/onboarding': (context) =>
             OnboardingScreen(navigator: mainNavigatorKey),
-        'home': (context) => HomePage(),
+        '/home': (context) => HomePage(),
       },
     );
   }
