31f6da3528ace195fba2788353bf95317e853bb1
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-26 16:24:30 +0200

    Fixed theme of text

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -27,8 +27,8 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF0E86D4).withOpacity(0.8),
         cardColor: const Color(0xFFE3E7EE),
         textTheme: TextTheme(
-          bodyMedium: const TextStyle(fontFamily: 'Montserrat', fontSize: 14.0),
-          bodyLarge: TextStyle(fontFamily: 'Montserrat', fontSize: 16.0, color: Colors.grey[700], fontWeight: FontWeight.bold),
+          bodyMedium: const TextStyle(fontFamily: 'Montserrat', fontSize: 15.0),
+          bodyLarge: TextStyle(fontFamily: 'Montserrat', fontSize: 18.0, color: Colors.grey[700], fontWeight: FontWeight.bold),
         ),
       ),
       initialRoute: '/authentication',
