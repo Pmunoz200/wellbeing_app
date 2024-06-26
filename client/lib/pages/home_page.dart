63d8627dcaeadf9cffa47cf2a3d6fae638eecec0
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-06-26 21:37:30 +0200

    Moved appbar widget to different file

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Import the intl package
+import 'home_page/home_app_bar.dart';
 import 'package:gemini_folder/pages/home_page/exercise_widget.dart';
 import 'package:gemini_folder/pages/home_page/food_widget.dart';
 import 'package:gemini_folder/pages/home_page/home_widget.dart';
@@ -53,55 +53,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        // Handle previous date logic
-                        _goBackOneDayOnDate();
-                      },
-                      color: Colors.black,
-                    ),
-          title: Center(
-            child: Text(
-                              DateFormat('EEEE, MMM d')
-                                  .format(_selectedDate)
-                                  .toString(), // Fix the DateTime.format method
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                      // Don't show 4 the current date
-            ),
-          ),
-          actions: [
-            _selectedDate.isBefore(
-                            startOfTomorrow.subtract(const Duration(days: 1)))
-                        ? IconButton(
-                            icon: const Icon(Icons.arrow_forward),
-                            onPressed: () {
-                              // Handle next date logic
-                              _selectedDate == DateTime.now()
-                                  ? null
-                                  : _advanceOneDayOnDate();
-                            },
-                            color: Colors.black,
-                          )
-                        : const SizedBox(
-                            width: 48,
-                          ),
-            Container(
-                width: 40,
-                height: 40,
-                margin: EdgeInsets.zero,
-                child: IconButton(
-                  onPressed: () {},
-                  constraints: const BoxConstraints(),
-                  icon: Image.asset("assets/icon_profile.png")
-                ),
-              )],
-          backgroundColor: Theme.of(context).primaryColor,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: HomeAppBar(advanceOneDayOnDate: _advanceOneDayOnDate, goBackOneDayOnDate: _goBackOneDayOnDate, selectedDate: _selectedDate),
         ),
         body: Container(
           decoration: BoxDecoration(
