9ff6c0684cbeeee5e55d24b90249ab0f3fa58fbb
JuanJoseJ <s301352@polito.studenti.it>
2024-06-28 10:19:07 -0500

    Initial base integration of chat input with txt containers. Add messages to the list.

--- client/lib/pages/home_page/home_widget.dart
+++ client/lib/pages/home_page/home_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_folder/chat/message_class.dart';
+import 'package:gemini_folder/chat/message_input.dart';
 import 'package:gemini_folder/pages/widgets/text_response_container.dart';
-
-const String tryText =
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap.";
-const List<String> textList = [tryText, "1" + tryText, "2" + tryText];
 
 class HomeWidgetPage extends StatefulWidget {
   HomeWidgetPage({Key? key}) : super(key: key);
@@ -13,6 +11,10 @@ class HomeWidgetPage extends StatefulWidget {
 }
 
 class _HomeWidgetPageState extends State<HomeWidgetPage> {
+  String tryText =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap.";
+  List<String> textList = [];
+
   bool _isSummaryExpanded = false;
   bool _isSuggestionsExpanded = false;
   void toggleSummary() {
@@ -35,26 +37,48 @@ class _HomeWidgetPageState extends State<HomeWidgetPage> {
     return _isSuggestionsExpanded;
   }
 
+  void _sendMessage(MessageObject message) {
+    // _chatService.addMessage(message);
+    String textMessage = message.textMessage ?? "No text message";
+    setState(() {
+      textList.add(textMessage);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    textList = [tryText, "1" + tryText, "2" + tryText];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _isSuggestionsExpanded
-            ? Container()
-            : TextResponseContainer(
-                texts: textList,
-                title: "Summary",
-                getIsExpanded: getSuggestionsExpandedValue,
-                callback: toggleSummary),
-        _isSummaryExpanded
-            ? Container()
-            : TextResponseContainer(
-                texts: textList,
-                title: "Suggestions",
-                getIsExpanded: getSuggestionsExpandedValue,
-                callback: toggleSuggestions,
-              ),
+        Expanded(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+              _isSuggestionsExpanded
+                  ? Container()
+                  : TextResponseContainer(
+                      texts: textList,
+                      title: "Summary",
+                      getIsExpanded: getSuggestionsExpandedValue,
+                      callback: toggleSummary),
+              _isSummaryExpanded
+                  ? Container()
+                  : TextResponseContainer(
+                      texts: textList,
+                      title: "Suggestions",
+                      getIsExpanded: getSuggestionsExpandedValue,
+                      callback: toggleSuggestions,
+                    ),
+            ])),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+          child: MessageInput(sendMessageFunction: _sendMessage),
+        ),
       ],
     );
   }
