2bd0f1c9d96879f628fac654291938c299b11957
JuanJoseJ <s301352@polito.studenti.it>
2024-06-29 09:53:27 -0500

    Modify: Change color of the text input container

--- client/lib/chat/message_input.dart
+++ client/lib/chat/message_input.dart
@@ -276,7 +276,7 @@ class _MessageInputState extends State<MessageInput> {
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.all(Radius.circular(15)),
-                color: Theme.of(context).colorScheme.primaryContainer,
+                color: Theme.of(context).colorScheme.surfaceContainer,
               ),
               child: Row(
                 children: <Widget>[
