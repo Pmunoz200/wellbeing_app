40dcca1a65d3f3c24ee67c4ec1d650e073981dd9
JuanJoseJ <s301352@polito.studenti.it>
2024-07-06 10:04:38 -0500

    Initial commit with the base functions to consume the endpoint

---
+++ client/lib/services/gemini_service/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'conversation_model.dart';
+
+class ApiService {
+  final String baseUrl;
+
+  // Constructor to initialize the base URL
+  ApiService({required this.baseUrl});
+
+  // Method to get a response from the server
+  Future<ConversationModel> getResponse(String uid, String query, DateTime date) async {
+    // Format the date to the required string format
+    final String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(date);
+    // Make a GET request to the server
+    final response = await http.get(Uri.parse('$baseUrl/get_response?uid=$uid&query=$query&date=$formattedDate'));
+
+    // Check if the response status code is 200 (OK)
+    if (response.statusCode == 200) {
+      // Parse the JSON response and return the ConversationModel
+      return ConversationModel.fromJson(jsonDecode(response.body));
+    } else {
+      // Throw an exception if the request failed
+      throw Exception('Failed to load conversation');
+    }
+  }
+}
