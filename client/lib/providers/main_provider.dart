5c30d4e48366e93e06483f99838732af8378ab0b
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-07-07 22:13:37 +0200

    Added try/catch block

--- client/lib/providers/main_provider.dart
+++ client/lib/providers/main_provider.dart
@@ -7,12 +7,18 @@ class MainProvider with ChangeNotifier {
   Profile? userProfile;
 
   // ConversationModel object to store the current conversation
-  ConversationModel? currentConversations = null;
+  ConversationModel? currentConversations = ConversationModel(
+          conversation: [],
+        );
 
   // Method to fetch the user conversation for a specific date
   void fetchMessagesAndUpdateThem(String uid, DateTime date) async {
-    final ApiService apiService = ApiService(baseUrl: 'http://localhost:5000');
-    currentConversations = await apiService.getUserMessagesByDate(uid, date);
-    notifyListeners();
+    try{
+      final ApiService apiService = ApiService(baseUrl: 'http://127.0.0.1:5001/wellbeing-app-d5a53/us-central1');
+      currentConversations = await apiService.getUserMessagesByDate(uid, date);
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
   }
 }
