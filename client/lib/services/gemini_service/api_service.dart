546366fabf42012d1a244e9e2eca7958666133a9
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-07-07 18:47:38 +0200

    Added function to retriever messages from a certain date

--- client/lib/services/gemini_service/api_service.dart
+++ client/lib/services/gemini_service/api_service.dart
@@ -25,4 +25,21 @@ class ApiService {
       throw Exception('Failed to load conversation');
     }
   }
+  // Method to get a list of messages on a specific date from the server
+  Future<ConversationModel> getUserMessagesByDate(String uid, String query, DateTime date) async {
+    // Format the date to the required string format
+    final String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(date);
+    // Make a GET request to the server
+    final response = await http.get(Uri.parse('$baseUrl/get_user_messages_by_date?uid=$uid&date=$formattedDate'));
+
+    // Check if the response status code is 200 (OK)
+    if (response.statusCode == 200) {
+      // Parse the JSON response and return the ConversationModel
+      final conversationList = ConversationModel.fromJson(jsonDecode(response.body));
+      return conversationList;
+    } else {
+      // Throw an exception if the request failed
+      throw Exception('Failed to load conversation');
+    }
+  }
 }
