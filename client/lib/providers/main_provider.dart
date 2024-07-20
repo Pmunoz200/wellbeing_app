5b67526feea4c5216e7bf6e7470fa078160af920
Pablo Muñoz <pablo.munozs@hotmail.com>
2024-07-20 22:50:47 +0200

    Retriever texts from API

--- client/lib/providers/main_provider.dart
+++ client/lib/providers/main_provider.dart
@@ -3,6 +3,7 @@ import 'package:gemini_folder/pages/user_authentication_page/profile_class.dart'
 import 'package:gemini_folder/services/gemini_service/api_service.dart';
 import 'package:gemini_folder/services/gemini_service/conversation_model.dart';
 
+final String BASEURL = 'http://127.0.0.1:5001/wellbeing-app-d5a53/us-central1';
 class MainProvider with ChangeNotifier {
   Profile? userProfile;
 
@@ -14,11 +15,27 @@ class MainProvider with ChangeNotifier {
   // Method to fetch the user conversation for a specific date
   void fetchMessagesAndUpdateThem(String uid, DateTime date) async {
     try{
-      final ApiService apiService = ApiService(baseUrl: 'http://127.0.0.1:5001/wellbeing-app-d5a53/us-central1');
+      final ApiService apiService = ApiService(baseUrl: BASEURL);
       currentConversations = await apiService.getUserMessagesByDate(uid, date);
       notifyListeners();
     } catch (e) {
       print(e);
     }
   }
+
+  List<String> getHomeWidgetTexts() {
+    if (currentConversations != null) {
+      print(currentConversations!.toJson());
+      final modelMessageList = currentConversations!.conversation
+          .where((element) => element.role == "model");
+      print(modelMessageList);
+      List<String> returnMessageList = [];
+      for (var message in modelMessageList) {
+        returnMessageList.add(message.content.summary);
+      }
+      return returnMessageList;
+    }
+    return [];
+  }
+
 }
